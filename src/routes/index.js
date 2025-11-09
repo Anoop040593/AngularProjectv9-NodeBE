01894c378f754244927afce0297b9b489b7f082a
Anoop Pemmaraju <anuppemmaraju@outlook.com>
2025-11-09 18:35:12 +0530

    new Backend Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,15 @@ import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { addViewToListingRoutes } from "./addViewListing";
 import { getUserListingsRoute } from "./getUserListings";
-export default [getAllListingsRoute, 
+import { createNewListingRoute } from "./createNewListings";
+import { updateListingRoute } from "./updateListngs";
+import { deleteListingRoute } from "./deleteListings";
+
+export default [
+    getAllListingsRoute, 
     getListingRoute, 
-    addViewToListingRoutes, getUserListingsRoute   ]
+    addViewToListingRoutes, 
+    getUserListingsRoute,
+    createNewListingRoute,
+    updateListingRoute,
+    deleteListingRoute ]
